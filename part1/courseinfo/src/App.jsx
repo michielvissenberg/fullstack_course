4c0e1ccffbb75a81bedb7c4fb369185e1ed610cb
michielvissenberg <michiel@vissenberg.be>
2025-02-10 20:03:27 +0100

    exercise 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const Header = (props) => {
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <>
+      <p>
+        {props.partNum} {props.exercisesNum}
       </p>
     </>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <>
+      <Part partNum={props.part1} exercisesNum={props.exercises1} />
+      <Part partNum={props.part2} exercisesNum={props.exercises2} />
+      <Part partNum={props.part3} exercisesNum={props.exercises3} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return(
+  <>
+      <p>
+        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
   )
 }
 
